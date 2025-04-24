e8749642d3deab7df27ecb73ab5b15ee55f661c9
I Made Prabawa Sandhi Gotama <121427896+PlaneFly545@users.noreply.github.com>
2025-04-24 23:31:31 +0800

    Update uhi_fetcher.py

--- uhi_fetcher.py
+++ uhi_fetcher.py
@@ -4,13 +4,18 @@ from datetime import timedelta, datetime
 import json
 import os
 
-# Dictionary dengan kode wilayah
 kode_wilayah_dict = {
     "u1_tanah_tinggi": "36.71.01.1003",
     "r1_1_kemang": "32.01.12.2003",
     "r1_2_parung": "32.01.10.2001",
-    "r3_3_tanjurhalang": "32.01.37.2001"
+    "r1_3_tanjurhalang": "32.01.37.2001",
+    
+    "u2_tuban": "51.03.01.1001",
+    "r2_1_baler_bale_agung": "51.01.01.1006",
+    "r2_2_medewi": "51.01.03.2001",
+    "r2_3_yehembang": "51.01.02.2008"
 }
+
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
@@ -50,19 +55,35 @@ for key, kode_wilayah_4 in kode_wilayah_dict.items():
         print(f"[ERROR] Gagal ambil data untuk {key}: {e}")
 
 uhi_data = {
-    "uhi_tating_kemang": {},
-    "uhi_tating_parung": {},
-    "uhi_tating_tanjurhalang": {}
+    "Tanah Tinggi": {
+        "kemang": {},
+        "parung": {},
+        "tanjurhalang": {}
+    },
+    "Tuban": {
+        "baler_bale_agung": {},
+        "medewi": {},
+        "yehembang": {}
+    }
 }
 
 for tanggal in tanggal_harian:
     try:
+        # Tanah Tinggi
         suhu_u1 = wilayah_data["u1_tanah_tinggi"][tanggal]
-        uhi_data["uhi_tating_kemang"][tanggal] = suhu_u1 - wilayah_data["r1_1_kemang"].get(tanggal, suhu_u1)
-        uhi_data["uhi_tating_parung"][tanggal] = suhu_u1 - wilayah_data["r1_2_parung"].get(tanggal, suhu_u1)
-        uhi_data["uhi_tating_tanjurhalang"][tanggal] = suhu_u1 - wilayah_data["r3_3_tanjurhalang"].get(tanggal, suhu_u1)
+        uhi_data["Tanah Tinggi"]["kemang"][tanggal] = suhu_u1 - wilayah_data["r1_1_kemang"].get(tanggal, suhu_u1)
+        uhi_data["Tanah Tinggi"]["parung"][tanggal] = suhu_u1 - wilayah_data["r1_2_parung"].get(tanggal, suhu_u1)
+        uhi_data["Tanah Tinggi"]["tanjurhalang"][tanggal] = suhu_u1 - wilayah_data["r1_3_tanjurhalang"].get(tanggal, suhu_u1)
+
+        # Tuban
+        suhu_u2 = wilayah_data["u2_tuban"][tanggal]
+        uhi_data["Tuban"]["baler_bale_agung"][tanggal] = suhu_u2 - wilayah_data["r2_1_baler_bale_agung"].get(tanggal, suhu_u2)
+        uhi_data["Tuban"]["medewi"][tanggal] = suhu_u2 - wilayah_data["r2_2_medewi"].get(tanggal, suhu_u2)
+        uhi_data["Tuban"]["yehembang"][tanggal] = suhu_u2 - wilayah_data["r2_3_yehembang"].get(tanggal, suhu_u2)
+
     except KeyError as e:
         print(f"[WARNING] Data suhu tidak lengkap untuk tanggal {tanggal}: {e}")
+
 
 with open("uhi_result.json", "w") as json_file:
     json.dump(uhi_data, json_file, indent=4)
